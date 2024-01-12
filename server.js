71206595683809708574c0c513da4afff38c92e5
unknown <sandeepmauryaworld>
2024-01-12 21:18:58 +0530

    backend done

--- server.js
+++ server.js
@@ -2,27 +2,30 @@ import express from "express";
 import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
-
-import cors from "cors";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
 
-//configure env
+import categoryRoutes from "./routes/categoryRoutes.js";
+// import productRoutes from "./routes/productRoutes.js";
+import cors from "cors";
+
+// //configure env
 dotenv.config();
 
-// databse config
+// //databse config
 connectDB();
 
-//rest object
+// //rest object
 const app = express();
 
-//middelwares
+// //middelwares
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-//routes
-// app.use("/api/v1/auth", authRoutes);
-// app.use("/api/v1/category", categoryRoutes);
+// //routes
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
 // app.use("/api/v1/product", productRoutes);
 
 //rest api
@@ -36,7 +39,7 @@ const PORT = process.env.PORT || 8080;
 //run listen
 app.listen(PORT, () => {
   console.log(
-    `Server Running on mode  on ${process.env.PORT} port ${PORT}`.bgCyan
+    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
       .white
   );
 });
